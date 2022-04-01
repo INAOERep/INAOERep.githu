90e32a5032e4c8154d3cc0cc3801e5e54d7d34c5
INAOERep <102568992+INAOERep@users.noreply.github.com>
2022-04-01 02:55:08 -0600

    Update particles.js

--- _js/vendors/particles.js
+++ _js/vendors/particles.js
@@ -21,7 +21,7 @@ var pJS = function(tag_id, params) {
     },
     particles: {
       number: {
-        value: 400,
+        value: 500,
         density: {
           enable: true,
           value_area: 800
