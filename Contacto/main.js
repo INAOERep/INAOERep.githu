81cdf535f100a7554efde7ab0943e532ab157216
AsteroidesINAOE <104033354+AsteroidesINAOE@users.noreply.github.com>
2022-09-15 20:38:50 -0500

    update

---
+++ Contacto/main.js
@@ -0,0 +1,47 @@
+function copiar1() {
+  var content1 = document.getElementById('correo1');
+  content1.select();
+  document.execCommand('copy');
+}
+
+function copiar2() {
+  var content2 = document.getElementById('correo2');
+  content2.select();
+  document.execCommand('copy');
+}
+
+function copiar3() {
+  var content3 = document.getElementById('correo3');
+  content3.select();
+  document.execCommand('copy');
+}
+
+function copiar4() {
+  var content4 = document.getElementById('correo4');
+  content4.select();
+  document.execCommand('copy');
+}
+
+function copiar5() {
+  var content5= document.getElementById('correo5');
+  content5.select();
+  document.execCommand('copy');
+}
+
+function copiar6() {
+  var content6= document.getElementById('correo6');
+  content6.select();
+  document.execCommand('copy');
+}
+
+function copiar7() {
+  var content7= document.getElementById('correo7');
+  content7.select();
+  document.execCommand('copy');
+}
+
+function copiar8() {
+  var content8= document.getElementById('correo8');
+  content8.select();
+  document.execCommand('copy');
+}
