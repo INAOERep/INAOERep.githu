be781eb321f1a72b4928adfd92b4507e29be6519
Alejandro Yg <44517806+aleyg@users.noreply.github.com>
2022-10-03 09:39:20 -0500

    update

--- infografias/acercamientos/js/imagenes.js
+++ infografias/acercamientos/js/imagenes.js
@@ -1,52 +1,18 @@
-const grid = new Muuri('.grid', {
-	layout: {
-		rounding: false
-	}
-});
+var modal1 = document.getElementById("myModal1");
 
-window.addEventListener('load', () => {
-	grid.refreshItems().layout();
-	document.getElementById('grid').classList.add('imagenes-cargadas');
+const markers = document.querySelectorAll("#map-svg image");
+var modalImg1 = document.getElementById("img1");
 
-	// Agregamos los listener de los enlaces para filtrar por categoria.
-	const enlaces = document.querySelectorAll('#categorias a');
-	enlaces.forEach((elemento) => {
-		elemento.addEventListener('click', (evento) => {
-			evento.preventDefault();
-			enlaces.forEach((enlace) => enlace.classList.remove('activo'));
-			evento.target.classList.add('activo');
+markers.forEach(marker => marker.addEventListener("click",setModal));
 
-			const categoria = evento.target.innerHTML.toLowerCase();
-			categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
-		});
-	});
+function setModal(){
+	modalImg1.src = './img/${this.dataset.src}';
+	modal1.style.display = "block";
+}
 
-	// Agregamos el listener para la barra de busqueda
-	document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
-		const busqueda = evento.target.value;
-		grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
-	});
+var span1 = document.querySelector(".close1");
 
-	// Agregamos listener para las imagenes
-	const overlay = document.getElementById('overlay');
-	document.querySelectorAll('.grid .item img').forEach((elemento) => {
-		elemento.addEventListener('click', () => {
-			const ruta = elemento.getAttribute('src');
-			const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
-
-			overlay.classList.add('activo');
-			document.querySelector('#overlay img').src = ruta;
-			document.querySelector('#overlay .descripcion').innerHTML = descripcion;
-		});
-	});
-
-	// Eventlistener del boton de cerrar
-	document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
-		overlay.classList.remove('activo');
-	});
-
-	// Eventlistener del overlay
-	overlay.addEventListener('click', (evento) => {
-		evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
-	});
-});
+span1.onclick = function() {
+	modal1.style.display = "none";
+	modalImg1.src = "";
+}
